1cca48f035cec8ae1e4f8aa8bf2302ea2bd2dd81
Shriyash1107 <shriyashsureshpatil@gmail.com>
2026-03-17 07:49:42 +0530

    added vercel link to main

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,10 @@ app = FastAPI(title="Contract Risk API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:5174"],
+    allow_origins=["http://localhost:5173", "http://localhost:5174",
+     "https://contractlens.vercel.app",
+     "*"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
