122a02827fe5a08fc93544e803ffa975cbc76e9b
vikaspatel <vikaspatel11245@gmail.com>
2026-03-18 22:27:42 +0530

    no bacnend

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from backend.routers.analyze import router as analyze_router
+from routers.analyze import router as analyze_router  # ✅ fixed
 
 app = FastAPI(title="Contract Risk Heatmap API")
 
@@ -10,8 +10,6 @@ app.add_middleware(
         "http://localhost:5173",
         "https://contractlens-ed9t1yvo3-vikaspatel11245s-projects.vercel.app",
         "https://contractlens-ai.vercel.app"
-        "https://vercel.com/vikaspatel11245s-projects/contractlens/GRqr6YM5LH7icRvT8rU4PH3cNKc6"
-        "https://contractlens-7h94w5n20-vikaspatel11245s-projects.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
